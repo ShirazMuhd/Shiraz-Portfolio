85a5a320e8f6fc5e344321bb44b355412719d429
ShirazMuhd <shirazmuhdofficial@gmail.com>
2021-05-12 12:30:20 +0530

    year added to footer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-
 //get current year for footer
-function getYear() {
-  var d = new Date();
-  var year = d.getFullYear();
-  document.getElementById("year").innerHTML = year;
-}
 
-
+var d=new Date()
+var year = d.getFullYear()
+document.getElementById('year').innerHTML=year
